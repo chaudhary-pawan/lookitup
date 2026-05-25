f65508b517279aa9ec9a3affd9c7d234c81099b2
chaudhary-pawan <Pc1498180@gmail.com>
2026-05-25 18:43:03 +0530

    frontend: disable colored glowing background orbs

--- frontend/src/components/Orbs.jsx
+++ frontend/src/components/Orbs.jsx
@@ -1,9 +1,3 @@
 export default function Orbs() {
-  return (
-    <div className="orbs" aria-hidden="true">
-      <div className="orb orb-1" />
-      <div className="orb orb-2" />
-      <div className="orb orb-3" />
-    </div>
-  );
+  return null;
 }
